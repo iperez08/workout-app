9f8f097986a26380b29a2f016190e3f52cfe80d7
Ismael P <iperez08@gmail.com>
2024-08-12 00:18:15 -0400

    render exercises by components page

--- controllers/program.js
+++ controllers/program.js
@@ -2,6 +2,7 @@ import express from "express"
 const router = express.Router()
 import Week from "../models/week.js"
 import Program from "../models/program.js"
+import Workout from "../models/workout.js"
 import { createProgram, updateProgramWithWeeks } from "../models/program-rendering/program-builder.js"
 import { updateWeeksWithWorkouts } from "../models/program-rendering/week-builder.js"
 import { updateWorkoutWithComponents } from "../models/program-rendering/workout-builders.js"
@@ -59,7 +60,7 @@ router.post(`/:programID/weeks/:weekID/workouts/new`, async (req, res) => {
         const weeks = await Promise.all(weekPromises)
         const updatePromises = weeks.map((week, idx) => updateWorkoutWithComponents(week.workouts, allComponentNames[idx]))
         await Promise.all(updatePromises)
-        res.redirect(`/${req.params.programID}/weeks/workouts/exercises.new.ejs`)
+        res.redirect(`/programs/${req.params.programID}/weeks/workouts/exercises/new`)
     } catch (error) {
         console.log(`error creating workouts: ${error}`)
     }
@@ -67,27 +68,32 @@ router.post(`/:programID/weeks/:weekID/workouts/new`, async (req, res) => {
 
 router.get('/:programID/weeks/workouts/exercises/new', async (req, res) => {
     const program = await Program.findById(req.params.programID)
-    const dayOne = program.weeks[0].workouts[0]
-    const dayTwo = program.weeks[0].workouts[1]
-    const dayThree = program.weeks[0].workouts[2]
-    const dayFour = program.weeks[0].workouts[3]
-    const allWorkouts = [dayOne, dayTwo, dayThree, dayFour]
+    const weekOne = await Week.findById(program.weeks[0])
+    const workoutPromises = weekOne.workouts.map((workoutId) => Workout.findById(workoutId))
+    const allWorkouts = await Promise.all(workoutPromises)
     const warmupA = ["Cossack Squat", "Ab Wheel", "Bulgarian One-Leg Squat", "Band Pull-Aparts"]
     const warmupB = ["Side Lunge", "Push Ups", "Forward Lunges", "Shoulder Dislocates"]
-    const mainLifts = ["Barbell Squat", "Barbell Deadlift", "Barbell Bench Press", "Barbell Overhead Press"]
-    const supplementalLifts = [" Front Barbell Squat", "Barbell Romanian Deadlift", "Barbell Decline Bench Press", "Barbell Incline Bench Press"]
+    const mainExercises = ["Barbell Squat", "Barbell Deadlift", "Barbell Bench Press", "Barbell Overhead Press"]
+    const supplementalExercises = [" Front Barbell Squat", "Barbell Romanian Deadlift", "Barbell Decline Bench Press", "Barbell Incline Bench Press"]
     const accessoriesA = ["Tricep Extension", "Inverted Rows", "Hanging Leg Raise"]
     const accessoriesB = ["Push Ups", "Dumbbell Curls", "Hammer Curls", "Ab Wheel"]
-    res.render(`program/week/workout/exercise.new.ejs`, {
-        allWorkouts,
-        warmupA,
-        warmupB,
-        mainLifts,
-        supplementalLifts,
-        accessoriesA,
-        accessoriesB
+    res.render(`program/week/workout/exercise/new.ejs`, {
+        programID: req.params.programID,
+        allWorkouts: allWorkouts,
+        warmupA: warmupA,
+        warmupB: warmupB,
+        mainExercises: mainExercises,
+        supplementalExercises: supplementalExercises,
+        accessoriesA: accessoriesA,
+        accessoriesB: accessoriesB
     })
 })
+
+router.post('/:programID/weeks/workouts/exercises/new', async (req, res) => {
+    
+}
+
+)
 
 
 export default router

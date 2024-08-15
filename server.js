dfd2c680a9fc09f4faf9223dad04949fc176dcee
Ismael P <iperez08@gmail.com>
2024-08-15 12:02:18 -0400

    change all double quotations to single quotations

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-import dotenv from "dotenv"
+import dotenv from 'dotenv'
 dotenv.config()
-import express from "express"
+import express from 'express'
 const app = express()
 
-import mongoose, { mongo } from "mongoose"
-import methodOverride from "method-override"
-import session from "express-session"
+import mongoose, { mongo } from 'mongoose'
+import methodOverride from 'method-override'
+import session from 'express-session'
 
-import authCtrl from "./controllers/auth.js"
-import userCtrl from "./controllers/user.js"
-import programCtrl from "./controllers/program.js"
-import cycleCtrl from "./controllers/cycle.js"
+import authCtrl from './controllers/auth.js'
+import userCtrl from './controllers/user.js'
+import programCtrl from './controllers/program.js'
+import cycleCtrl from './controllers/cycle.js'
 
-const port = process.env.PORT || "3000"
+const port = process.env.PORT || '3000'
 
 mongoose.connect(process.env.MONGODB_URI)
 
-app.use(express.static("contents"))
+app.use(express.static('contents'))
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
+app.use(methodOverride('_method'))
 
 app.use(
     session({
@@ -28,19 +28,21 @@ app.use(
     })
 )
 
-app.get("/", (req, res) => {
-    res.render("landing.ejs")
+app.get('/', (req, res) => {
+    res.render('landing.ejs')
 })
 
-app.use("/auth", authCtrl)
+app.set('view engine', 'ejs')
+
+app.use('/auth', authCtrl)
 app.use('/user', programCtrl)
 app.use('/user', userCtrl)
-app.use("/user", cycleCtrl)
+app.use('/user', cycleCtrl)
 
 app.listen(port, () => {
     console.log(`Express app is ready on port ${port}.`)
 })
 
-mongoose.connection.on("connected", () => {
+mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connect.name}`)
 })
